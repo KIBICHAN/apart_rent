c3f255bd5702ddc50f16c8a4564e7293b2b89dec
SunFlower2000 <52041113+KIBICHAN@users.noreply.github.com>
2022-06-26 16:27:33 +0700

    I have fake my first API :)

---
+++ lib/repository/api/api.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:apart_rent/models/rent_post.dart';
+import 'package:apart_rent/repository/api/apiUrl.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<RentPost>> fetchAllData() async {
+  final response = await http.get(Uri.parse(apiUrl));
+  if(response.statusCode == 200){
+    return (jsonDecode(response.body) as List).map((e) => RentPost.fromJson(e)).toList();
+  }else{
+    throw Exception('Fail to load API');
+  }
+}
